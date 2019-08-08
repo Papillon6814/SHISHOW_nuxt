1784b0c5f076df6424f919ce9feac5bcfb410706
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-08-09 01:04:14 +0900

    Create EmptyNavigationBar.vue

---
+++ shishow/components/EmptyNavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="header">
+    <img class="logo" src="../assets/image/logoShishow.png" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'emptyNavi'
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+#header {
+  position: fixed;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: $header_height;
+
+  background-color: $header_color;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+}
+
+</style>
